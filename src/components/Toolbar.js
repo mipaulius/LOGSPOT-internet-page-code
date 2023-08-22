f5584f154f7b72d827931c3e5bd312bc827a0d69
mipaulius <mi.paulius@gmail.com>
2023-08-22 18:05:44 +0300

    Header design

--- src/components/Toolbar.js
+++ src/components/Toolbar.js
@@ -1,18 +1,40 @@
-import React from "react";
-import {Link} from "react-router-dom";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import './Toolbar.css';
+import SignUpButton from '../images/button.svg';
+import PushedButton from '../images/button-pushed.svg';
+import LogspotLogo from '../images/logo.svg'
 
 const Toolbar = () => {
-    return (
-        <div className="header">
-          <Link to="/">Index</Link>
-          <Link to="/home">For Talents</Link>
-          <Link to="/it-hiring">For Employers</Link>
-          <Link to="/about">About</Link>
-          <Link to="/sign-up">Sign Up</Link>
-        </div>
+  const [isButtonPushed, setIsButtonPushed] = useState(false);
 
-    );
+  const handleMouseDown = () => {
+    setIsButtonPushed(true);
+  };
 
+  const handleMouseUp = () => {
+    setIsButtonPushed(false);
+  };
+
+  return (
+    <div className="header">
+      <Link to="/"><div className="logspot-logo"><img
+          src={LogspotLogo}
+          alt="company logo"/></div></Link>
+      <Link to="/home"><div className="header-toolbar">For Talents</div></Link>
+      <Link to="/it-hiring"><div className="header-toolbar">For Employers</div></Link>
+      <Link to="/about"><div className="header-toolbar">About</div></Link>
+      <Link to="/sign-up">
+        <img
+          className="sign-up-button"
+          src={isButtonPushed ? PushedButton : SignUpButton}
+          alt="button for signing in"
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}
+        />
+      </Link>
+    </div>
+  );
 };
 
 export default Toolbar;
